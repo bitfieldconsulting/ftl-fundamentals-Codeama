e358ba7607cd06d87deac792bfd998adfb3f1fa8
Bukola Jimoh <bukola_jimoh@yahoo.co.uk>
2021-01-30 15:12:38 +0000

    more test cases

--- calculator_test.go
+++ calculator_test.go
@@ -40,16 +40,40 @@ func TestCalculator(t *testing.T) {
 			want: 4,
 		},
 		{
+			name: "Add two negative integers",
+			a:    -2, b: -2,
+			f:    calculator.Add,
+			want: -4,
+		},
+		{
+			name: "Add a positive integer and a negative fraction ",
+			a:    5, b: -2.48,
+			f:    calculator.Add,
+			want: 2.52,
+		},
+		{
 			name: "Subtract a larger integer from a smaller integer",
 			a:    3, b: 5,
 			f:    calculator.Subtract,
 			want: -2,
 		},
 		{
+			name: "Subtract a negative integer from a negative integer",
+			a:    -9, b: -5,
+			f:    calculator.Subtract,
+			want: -4,
+		},
+		{
 			name: "Multiply two positive integers",
 			a:    4, b: 20,
 			f:    calculator.Multiply,
 			want: 80,
+		},
+		{
+			name: "Multiply a positive fraction by a negative fraction",
+			a:    4, b: -9,
+			f:    calculator.Multiply,
+			want: -36,
 		},
 	}
 
