f110a34b1232cb2ba29571e4c4557e5705b959e6
Bukola Jimoh <bukola_jimoh@yahoo.co.uk>
2021-01-30 15:02:01 +0000

    use t.Run

--- calculator_test.go
+++ calculator_test.go
@@ -24,43 +24,41 @@ func TestSubtract(t *testing.T) {
 }
 
 type TestCase struct {
-	a    float64
-	b    float64
+	name string
+	a, b float64
 	f    func(float64, float64) float64
 	want float64
-	name string
 }
 
 func TestCalculator(t *testing.T) {
 	t.Parallel()
 	testCases := []TestCase{
 		{
-			a:    2,
-			b:    2,
+			name: "Add two positive integers",
+			a:    2, b: 2,
 			f:    calculator.Add,
 			want: 4,
-			name: "Add",
 		},
 		{
-			a:    3,
-			b:    5,
+			name: "Subtract a larger integer from a smaller integer",
+			a:    3, b: 5,
 			f:    calculator.Subtract,
 			want: -2,
-			name: "Subtract",
 		},
 		{
-			a:    4,
-			b:    20,
+			name: "Multiply two positive integers",
+			a:    4, b: 20,
 			f:    calculator.Multiply,
 			want: 80,
-			name: "Multiply",
 		},
 	}
 
 	for _, tc := range testCases {
-		got := tc.f(tc.a, tc.b)
-		if tc.want != got {
-			t.Errorf("%s: want %f, got %f", tc.name, tc.want, got)
-		}
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.f(tc.a, tc.b)
+			if tc.want != got {
+				t.Errorf("want %f, got %f", tc.want, got)
+			}
+		})
 	}
 }
